acf054b914b4bb7971582b89015df1a968259c73
Jerome Tan <DevJerome@iCloud.com>
2016-08-25 09:03:08 +0800

    Add demo app

---
+++ KeyboardAnimatableDemo/KeyboardAnimatableDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KeyboardAnimatableDemo
+//
+//  Created by Jerome Tan on 25/08/2016.
+//  Copyright © 2016 Jerome Tan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
