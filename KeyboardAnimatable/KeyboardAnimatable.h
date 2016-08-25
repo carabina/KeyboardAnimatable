affb7b77de6b1f3d1bd02118d80932f54ac1d6a8
Jerome Tan <DevJerome@iCloud.com>
2016-08-25 08:51:32 +0800

    Initial Commit

---
+++ KeyboardAnimatable/KeyboardAnimatable.h
@@ -0,0 +1,19 @@
+//
+//  KeyboardAnimatable.h
+//  KeyboardAnimatable
+//
+//  Created by Jerome Tan on 25/08/2016.
+//  Copyright © 2016 Jerome Tan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KeyboardAnimatable.
+FOUNDATION_EXPORT double KeyboardAnimatableVersionNumber;
+
+//! Project version string for KeyboardAnimatable.
+FOUNDATION_EXPORT const unsigned char KeyboardAnimatableVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KeyboardAnimatable/PublicHeader.h>
+
+
