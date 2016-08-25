cc21d2567c9be49774942fa4b68d6619b98215c8
Jerome Tan <DevJerome@iCloud.com>
2016-08-25 11:05:50 +0800

    Add documentation comments

--- KeyboardAnimatable/KeyboardAnimatable.swift
+++ KeyboardAnimatable/KeyboardAnimatable.swift
@@ -12,13 +12,28 @@ private var observersKey: Void?
 
 public protocol KeyboardAnimatable: class {
     
+    /// Perform animation when keyboard appears
+    ///
+    /// - note: `func enableKeyboardAnimation()` must be called previously
+    /// - important: `func disableKeyboardAnimation()` must be called to remove notification observers, this is typically done in `deinit` method
+    /// - parameter keyboardHeight: the height of keyboard when it is fully opened
+    /// - parameter duration:       the amount of time the keyboard takes to fully open
     func animateWhenKeyboardAppear(keyboardHeight keyboardHeight: CGFloat, duration: NSTimeInterval)
+    
+    /// Perform animation when keyboard disappears
+    ///
+    /// - important: `func disableKeyboardAnimation()` must be called to remove notification observers, this is typically done in `deinit` method
+    /// - parameter keyboardHeight: the height of keyboard when it is fully opened
+    /// - parameter duration:       the amount of time the keyboard takes to fully closed
     func animateWhenKeyboardDisappear(keyboardHeight keyboardHeight: CGFloat, duration: NSTimeInterval)
     
 }
 
 public extension KeyboardAnimatable where Self: UIViewController {
     
+    
+    /// Add observers to observe keboard notifications
+    /// - important: `func disableKeyboardAnimation()` must be called to remove notification observers, this is typically done in `deinit` method
     public func enableKeyboardAnimation() {
         let appearObserver = NSNotificationCenter.defaultCenter().addObserverForName(UIKeyboardWillShowNotification, object: nil, queue: nil) { [weak self] (notification) in
             guard let `self` = self else { return }
@@ -49,6 +64,7 @@ public extension KeyboardAnimatable where Self: UIViewController {
         observers = [appearObserver, dissappearObserver]
     }
     
+    /// Remove keyboard notification observers
     public func disableKeyboardAnimation() {
         for observer in observers {
             NSNotificationCenter.defaultCenter().removeObserver(observer)
